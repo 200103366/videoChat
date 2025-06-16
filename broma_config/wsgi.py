242a151c5f87861a43a2971c55c8573ebe4a37cd
200103366 <91107509+200103366@users.noreply.github.com>
2025-06-16 23:27:26 +0500

    wsgi.py

--- broma_config/wsgi.py
+++ broma_config/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "broma_config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "broma_config.settings")
 
 application = get_wsgi_application()
